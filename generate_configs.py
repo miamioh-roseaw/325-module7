521ffcc66810dc2391f3005e514684c60682fa50
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2025-07-29 16:04:04 -0400

    Create generate_configs.py

---
+++ generate_configs.py
@@ -0,0 +1,15 @@
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(loader=FileSystemLoader('.'))
+
+with open("device_data.yaml") as f:
+       devices = yaml.safe_load(f)["devices"]
+
+template = env.get_template("template.j2")
+
+for device in devices:
+       config = template.render(hostname=device["hostname"], ip=device["ip"])
+       with open(f"{device['hostname']}_config.txt", "w") as f:
+       f.write(config)
+       print(f"[GENERATED] {device['hostname']}_config.txt")
